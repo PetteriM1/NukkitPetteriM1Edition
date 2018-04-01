0e7d480f45c301a9602208b9adcf4bb915e6aff9
Petteri <petteri.malkavaara1@gmail.com>
2018-04-01 19:54:57 +0300

    Create EntityTameable.java

---
+++ src/main/java/cn/nukkit/entity/EntityTameable.java
@@ -0,0 +1,25 @@
+package cn.nukkit.entity;
+
+import cn.nukkit.Player;
+
+public interface Tameable {
+
+    public static final String NAMED_TAG_OWNER      = "Owner";
+
+    public static final String NAMED_TAG_OWNER_UUID = "OwnerUUID";
+
+    public static final String NAMED_TAG_SITTING    = "Sitting";
+
+    Player getOwner();
+
+    void setOwner(Player player);
+
+    String getOwnerUUID();
+
+    void setOwnerUUID(String uuid);
+
+    boolean isSitting();
+
+    void setSitting(boolean sitting);
+
+}
