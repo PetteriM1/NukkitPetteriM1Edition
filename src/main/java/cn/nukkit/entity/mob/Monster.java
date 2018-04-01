a8acc6976e0081af2ec9d30b94fed6326d806e65
Petteri <petteri.malkavaara1@gmail.com>
2018-04-01 20:40:52 +0300

    Create Monster.java

---
+++ src/main/java/cn/nukkit/entity/mob/Monster.java
@@ -0,0 +1,39 @@
+package cn.nukkit.entity.mob;
+
+import cn.nukkit.entity.Entity;
+
+public interface Monster {
+
+    void attackEntity(Entity player);
+
+    int getDamage();
+
+    int getDamage(Integer difficulty);
+
+    int getMinDamage();
+
+    int getMinDamage(Integer difficulty);
+
+    int getMaxDamage();
+
+    int getMaxDamage(Integer difficulty);
+
+    void setDamage(int damage);
+
+    void setDamage(int[] damage);
+
+    void setDamage(int damage, int difficulty);
+
+    void setMinDamage(int damage);
+
+    void setMinDamage(int[] damage);
+
+    void setMinDamage(int damage, int difficulty);
+
+    void setMaxDamage(int damage);
+
+    void setMaxDamage(int[] damage);
+
+    void setMaxDamage(int damage, int difficulty);
+
+}
