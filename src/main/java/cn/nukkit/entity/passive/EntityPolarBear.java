2ee8404a32c188361ec9237ff6c279c45220b9c0
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-05-19 20:32:56 +0300

    Damage for polar bears

--- src/main/java/cn/nukkit/entity/passive/EntityPolarBear.java
+++ src/main/java/cn/nukkit/entity/passive/EntityPolarBear.java
@@ -1,17 +1,22 @@
 package cn.nukkit.entity.passive;
 
+import cn.nukkit.entity.EntityCreature;
+import cn.nukkit.entity.mob.WalkingMonster;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
+import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.Player;
 import cn.nukkit.entity.Entity;
-import cn.nukkit.entity.passive.WalkingAnimal;
 import cn.nukkit.entity.EntityUtils;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.network.protocol.AddEntityPacket;
+import java.util.HashMap;
 
-public class EntityPolarBear extends WalkingAnimal {
+public class EntityPolarBear extends WalkingMonster {
 
     public static final int NETWORK_ID = 28;
+    int                     angry      = 0;
 
     public EntityPolarBear(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
@@ -56,7 +61,6 @@ public class EntityPolarBear extends WalkingAnimal {
         return 1.25;
     }
 
-    @Override
     public boolean isBaby() {
         return this.getDataFlag(DATA_FLAGS, Entity.DATA_FLAG_BABY);
     }
@@ -64,7 +68,90 @@ public class EntityPolarBear extends WalkingAnimal {
     @Override
     public void initEntity() {
         super.initEntity();
+        this.setDamage(new int[] { 0, 4, 6, 9 });
         this.setMaxHealth(30);
+        if (this.namedTag.contains("Angry")) {
+            this.angry = this.namedTag.getInt("Angry");
+        }
+    }
+
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+        this.namedTag.putInt("Angry", this.angry);
+    }
+
+    @Override
+    public boolean targetOption(EntityCreature creature, double distance) {
+        if (distance <= 100 && this.isAngry() && creature instanceof EntityPolarBear && !((EntityPolarBear) creature).isAngry()) {
+            ((EntityPolarBear) creature).setAngry(1000);
+        }
+        return this.isAngry() && super.targetOption(creature, distance);
+    }
+
+    @Override
+    public void attackEntity(Entity player) {
+        if (this.attackDelay > 10 && this.distanceSquared(player) < 1.44) {
+            this.attackDelay = 0;
+            HashMap<EntityDamageEvent.DamageModifier, Float> damage = new HashMap<>();
+            damage.put(EntityDamageEvent.DamageModifier.BASE, (float) this.getDamage());
+
+            if (player instanceof Player) {
+                @SuppressWarnings("serial")
+                HashMap<Integer, Float> armorValues = new HashMap<Integer, Float>() {
+
+                    {
+                        put(Item.LEATHER_CAP, 1f);
+                        put(Item.LEATHER_TUNIC, 3f);
+                        put(Item.LEATHER_PANTS, 2f);
+                        put(Item.LEATHER_BOOTS, 1f);
+                        put(Item.CHAIN_HELMET, 1f);
+                        put(Item.CHAIN_CHESTPLATE, 5f);
+                        put(Item.CHAIN_LEGGINGS, 4f);
+                        put(Item.CHAIN_BOOTS, 1f);
+                        put(Item.GOLD_HELMET, 1f);
+                        put(Item.GOLD_CHESTPLATE, 5f);
+                        put(Item.GOLD_LEGGINGS, 3f);
+                        put(Item.GOLD_BOOTS, 1f);
+                        put(Item.IRON_HELMET, 2f);
+                        put(Item.IRON_CHESTPLATE, 6f);
+                        put(Item.IRON_LEGGINGS, 5f);
+                        put(Item.IRON_BOOTS, 2f);
+                        put(Item.DIAMOND_HELMET, 3f);
+                        put(Item.DIAMOND_CHESTPLATE, 8f);
+                        put(Item.DIAMOND_LEGGINGS, 6f);
+                        put(Item.DIAMOND_BOOTS, 3f);
+                    }
+                };
+
+                float points = 0;
+                for (Item i : ((Player) player).getInventory().getArmorContents()) {
+                    points += armorValues.getOrDefault(i.getId(), 0f);
+                }
+
+                damage.put(EntityDamageEvent.DamageModifier.ARMOR,
+                        (float) (damage.getOrDefault(EntityDamageEvent.DamageModifier.ARMOR, 0f) - Math.floor(damage.getOrDefault(EntityDamageEvent.DamageModifier.BASE, 1f) * points * 0.04)));
+            }
+            player.attack(new EntityDamageByEntityEvent(this, player, EntityDamageEvent.DamageCause.ENTITY_ATTACK, damage));
+        }
+    }
+
+    public boolean isAngry() {
+        return this.angry > 0;
+    }
+
+    public void setAngry(int val) {
+        this.angry = val;
+    }
+
+    @Override
+    public boolean attack(EntityDamageEvent ev) {
+        super.attack(ev);
+
+        if (!ev.isCancelled()) {
+            this.setAngry(1000);
+        }
+        return true;
     }
 
     @Override
