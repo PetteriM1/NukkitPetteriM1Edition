51967ddbafd27b129f4c3b1b6a1545d586a90b3f
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-03-19 19:07:15 +0200

    1.10.0 new packets

---
+++ src/main/java/cn/nukkit/network/protocol/LecternUpdatePacket.java
@@ -0,0 +1,26 @@
+package cn.nukkit.network.protocol;
+
+import cn.nukkit.math.BlockVector3;
+
+public class LecternUpdatePacket extends DataPacket {
+
+    public int page;
+    public BlockVector3 blockPosition;
+    public boolean unknownBool;
+
+    @Override
+    public byte pid() {
+        return ProtocolInfo.LECTERN_UPDATE_PACKET;
+    }
+
+    @Override
+    public void decode() {
+        page = this.getByte();
+        blockPosition = this.getBlockVector3();
+        unknownBool = this.getBoolean();
+    }
+
+    @Override
+    public void encode() {
+    }
+}
---
+++ src/main/java/cn/nukkit/network/protocol/VideoStreamConnectPacket.java
@@ -0,0 +1,28 @@
+package cn.nukkit.network.protocol;
+
+public class VideoStreamConnectPacket extends DataPacket {
+
+    public static final byte ACTION_OPEN = 0;
+    public static final byte ACTION_CLOSE = 1;
+
+    public String address;
+    public float screenshotFrequency;
+    public byte action;
+
+    @Override
+    public byte pid() {
+        return ProtocolInfo.VIDEO_STREAM_CONNECT_PACKET;
+    }
+
+    @Override
+    public void decode() {
+    }
+
+    @Override
+    public void encode() {
+        this.reset();
+        this.putString(address);
+        this.putLFloat(screenshotFrequency);
+        this.putByte(action);
+    }
+}
