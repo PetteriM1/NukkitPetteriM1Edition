51675f5a0daa6785de70c55d517dd226ab738088
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-09-23 19:29:38 +0300

    Fix items disappearing in vehicle interactions

--- src/main/java/cn/nukkit/entity/item/EntityBoat.java
+++ src/main/java/cn/nukkit/entity/item/EntityBoat.java
@@ -348,7 +348,7 @@ public class EntityBoat extends EntityVehicle {
         }
 
         super.mountEntity(player);
-        return true;
+        return super.onInteract(player, item, clickedPos);
     }
 
     @Override
