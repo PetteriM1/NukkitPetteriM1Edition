1e08d461cd6625dd28f5a406372ee263f9196f24
Petteri <petteri.malkavaara1@gmail.com>
2020-03-12 18:46:50 +0200

    Stuff

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3934,8 +3934,9 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                 this.stopFishing(false);
             }
 
-            this.health = 0;
             this.extinguish();
+            this.removeAllEffects();
+            this.health = 0;
             this.scheduleUpdate();
 
             if (!ev.getKeepInventory() && this.level.getGameRules().getBoolean(GameRule.DO_ENTITY_DROPS)) {
