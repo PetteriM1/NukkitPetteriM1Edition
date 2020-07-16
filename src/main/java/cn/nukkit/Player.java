5529395c7ea7b58bd81ce782f931e252f88b9045
Alemiz112 <45739737+Alemiz112@users.noreply.github.com>
2020-07-16 15:20:56 +0200

    Do not call toLowerCase() multiple times

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -1959,14 +1959,15 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         }
 
         CompoundTag nbt;
-        File legacyDataFile = new File(server.getDataPath() + "players/" + this.username.toLowerCase() + ".dat");
+        String lowerName = this.username.toLowerCase();
+        File legacyDataFile = new File(server.getDataPath() + "players/" + lowerName + ".dat");
         File dataFile = new File(server.getDataPath() + "players/" + this.uuid.toString() + ".dat");
         if (this.server.savePlayerDataByUuid) {
             boolean dataFound = dataFile.exists();
             if (!dataFound && legacyDataFile.exists()) {
-                nbt = this.server.getOfflinePlayerData(this.username.toLowerCase(), false);
+                nbt = this.server.getOfflinePlayerData(lowerName, false);
                 if (!legacyDataFile.delete()) {
-                    this.server.getLogger().warning("Could not delete legacy player data for " + this.username.toLowerCase());
+                    this.server.getLogger().warning("Could not delete legacy player data for " + this.username);
                 }
             } else {
                 nbt = this.server.getOfflinePlayerData(this.uuid, !dataFound);
@@ -1976,7 +1977,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
             if (legacyMissing && dataFile.exists()) {
                 nbt = this.server.getOfflinePlayerData(this.uuid, false);
             } else {
-                nbt = this.server.getOfflinePlayerData(this.username.toLowerCase(), legacyMissing);
+                nbt = this.server.getOfflinePlayerData(lowerName, legacyMissing);
             }
         }
 
