d8487ae3aef6188daef7ae7aa2b63296ac9c4805
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-09-06 13:28:52 +0300

    Current version to 1.16.40

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -45,8 +45,8 @@ public interface ProtocolInfo {
             v1_16_100_52
     );
 
-    String MINECRAFT_VERSION = "v1.16.20";
-    String MINECRAFT_VERSION_NETWORK = "1.16.20";
+    String MINECRAFT_VERSION = "v1.16.40";
+    String MINECRAFT_VERSION_NETWORK = "1.16.40";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
