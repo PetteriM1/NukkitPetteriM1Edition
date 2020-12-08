f11df867fdda80909c562da82c64ad5af8d37896
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2020-12-08 20:34:22 +0200

    Bump version to 1.16.200

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -49,8 +49,8 @@ public interface ProtocolInfo {
             v1_16_100_52, v1_16_100, v1_16_200_51, v1_16_200, v1_16_210_50
     );
 
-    String MINECRAFT_VERSION = "v1.16.100";
-    String MINECRAFT_VERSION_NETWORK = "1.16.100";
+    String MINECRAFT_VERSION = "v1.16.200";
+    String MINECRAFT_VERSION_NETWORK = "1.16.200";
 
     byte BATCH_PACKET = (byte) 0xff;
     byte LOGIN_PACKET = 0x01;
