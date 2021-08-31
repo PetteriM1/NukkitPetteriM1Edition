704d5732d1f1d8e0ef44e9d261830d7fc4bed869
Petteri <26197131+PetteriM1@users.noreply.github.com>
2021-08-31 14:23:46 +0300

    Fix another typo in spawner code

--- src/main/java/cn/nukkit/utils/ClientChainData.java
+++ src/main/java/cn/nukkit/utils/ClientChainData.java
@@ -7,12 +7,14 @@ import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSObject;
-import com.nimbusds.jose.crypto.factories.DefaultJWSVerifierFactory;
+import com.nimbusds.jose.crypto.ECDSAVerifier;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.interfaces.ECPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
@@ -165,9 +167,10 @@ public final class ClientChainData implements LoginChainData {
     private UUID clientUUID;
     private String xuid;
 
-    private static PublicKey generateKey(String base64) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return KeyFactory.getInstance("EC").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(base64)));
+    private static ECPublicKey generateKey(String base64) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return (ECPublicKey) KeyFactory.getInstance("EC").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(base64)));
     }
+
     private String identityPublicKey;
 
     private long clientId;
@@ -229,8 +232,11 @@ public final class ClientChainData implements LoginChainData {
     }
 
     private void decodeChainData() {
-        Map<String, List<String>> map = GSON.fromJson(new String(bs.get(bs.getLInt()), StandardCharsets.UTF_8),
-                new MapTypeToken().getType());
+        int size = bs.getLInt();
+        if (size > 3000000) {
+            throw new IllegalArgumentException("The chain data is too big: " + size);
+        }
+        Map<String, List<String>> map = GSON.fromJson(new String(bs.get(size), StandardCharsets.UTF_8), new MapTypeToken().getType());
         if (map.isEmpty() || !map.containsKey("chain") || map.get("chain").isEmpty()) return;
         List<String> chains = map.get("chain");
 
@@ -261,20 +267,30 @@ public final class ClientChainData implements LoginChainData {
     }
 
     private static boolean verifyChain(List<String> chains) throws Exception {
-        PublicKey lastKey = null;
+        ECPublicKey lastKey = null;
         boolean mojangKeyVerified = false;
-        for (String chain: chains) {
+        for (String chain : chains) {
             JWSObject jws = JWSObject.parse(chain);
 
-            if (!mojangKeyVerified) {
-                // First chain should be signed using Mojang's private key. We'd be in big trouble if it leaked...
-                mojangKeyVerified = verify(MOJANG_PUBLIC_KEY, jws);
+            URI x5u = jws.getHeader().getX509CertURL();
+            if (x5u == null) {
+                return false;
             }
 
-            if (lastKey != null) {
-                if (!verify(lastKey, jws)) {
-                    throw new JOSEException("Unable to verify key in chain.");
-                }
+            ECPublicKey expectedKey = generateKey(x5u.toString());
+            // First key is self-signed
+            if (lastKey == null) {
+                lastKey = expectedKey;
+            } else if (!lastKey.equals(expectedKey)) {
+                return false;
+            }
+
+            if (!verify(lastKey, jws)) {
+                return false;
+            }
+
+            if (lastKey.equals(MOJANG_PUBLIC_KEY)) {
+                mojangKeyVerified = true;
             }
 
             String base64key = jws.getPayload().toJSONObject().getAsString("identityPublicKey");
@@ -286,8 +302,8 @@ public final class ClientChainData implements LoginChainData {
         return mojangKeyVerified;
     }
 
-    private static boolean verify(PublicKey key, JWSObject object) throws JOSEException {
-        return object.verify(new DefaultJWSVerifierFactory().createJWSVerifier(object.getHeader(), key));
+    private static boolean verify(ECPublicKey key, JWSObject object) throws JOSEException {
+        return object.verify(new ECDSAVerifier(key));
     }
 
     private static class MapTypeToken extends TypeToken<Map<String, List<String>>> {
