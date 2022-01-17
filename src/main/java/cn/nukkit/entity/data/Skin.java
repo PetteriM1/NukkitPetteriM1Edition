f5787e71eb5511ac5a5e5fb4529e5711066c1860
LT_Name <572413378@qq.com>
2022-01-17 21:53:54 +0800

    NukkitX PR 1948

--- src/main/java/cn/nukkit/entity/data/Skin.java
+++ src/main/java/cn/nukkit/entity/data/Skin.java
@@ -72,11 +72,19 @@ public class Skin {
     }
 
     private boolean isValidSkin() {
-        return skinId != null && !skinId.trim().isEmpty() && skinData != null && skinData.width >= 64 && skinData.height >= 32 && skinData.data.length >= SINGLE_SKIN_SIZE && (Server.getInstance().doNotLimitSkinGeometry || skinData.data.length <= MAX_DATA_SIZE);
+        return skinId != null && !skinId.trim().isEmpty() && skinId.length() < 100 &&
+                skinData != null && skinData.width >= 64 && skinData.height >= 32 &&
+                skinData.data.length >= SINGLE_SKIN_SIZE &&
+                (playFabId == null || playFabId.length() < 100) &&
+                (capeId == null || capeId.length() < 100) &&
+                (skinColor == null || skinColor.length() < 100) &&
+                (armSize == null || armSize.length() < 100) &&
+                (geometryDataEngineVersion == null || geometryDataEngineVersion.length() < 100) &&
+                (Server.getInstance().doNotLimitSkinGeometry || skinData.data.length <= MAX_DATA_SIZE);
     }
 
     private boolean isValidResourcePatch() {
-        if (skinResourcePatch == null) {
+        if (skinResourcePatch == null || skinResourcePatch.length() > 1000) {
             return false;
         }
         try {
